e714fb35c6c3fa7ef64419db7d8abd8d1cb9e355
Burchin <70316198+burcinismail8@users.noreply.github.com>
2021-05-23 12:00:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let randomNumber1 = Math.floor(Math.random() * 5);
+let randomNumber2 = Math.floor(Math.random() * 5);
+
+randomNumber1 += 1;
+randomNumber2 += 1;
+
+document
+  .querySelector(".img1")
+  .setAttribute("src", `images/dice` + randomNumber1 + `.png`);
+document
+  .querySelector(".img2")
+  .setAttribute("src", `images/dice` + randomNumber2 + `.png`);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
